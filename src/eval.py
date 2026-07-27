0b14d42e1f66b1eb58548ec7c7868b6b8646af1b
N.Gandhi <ng3n18@loginX001.cluster.local>
2026-07-27 11:26:18 +0100

    Cleaned up eval.py and made it use GPU resource

--- src/eval.py
+++ src/eval.py
@@ -3,32 +3,44 @@ from model import CatsDogsCNN
 from dataset import test_dataloader, train_dataloader
 from tqdm import tqdm
 
-model = CatsDogsCNN()
-model.load_state_dict(torch.load("catsdogs_model.pth"))
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print(f"Using device: {device}")
+
+if device.type == "cuda":
+    print(f"GPU: {torch.cuda.get_device_name(0)}")
+    
+model = CatsDogsCNN().to(device)
+model.load_state_dict(torch.load("catsdogs_model.pth", map_location=device))
 
 model.eval()
 
+# Training accuracy
 correct_predictions_training = 0
 total_predictions_training = 0
 
 with torch.no_grad():
     for batch_number, (images, labels) in enumerate(tqdm(train_dataloader)):
+        images = images.to(device)
+        labels = labels.to(device)
         output = model(images)
         predicted_classes = output.argmax(dim=1)
         correct_predictions_training += (predicted_classes == labels).sum().item()
         total_predictions_training += len(labels)
-        training_accuracy = 100 * correct_predictions_training / total_predictions_training
+training_accuracy = 100 * correct_predictions_training / total_predictions_training
 
 print(f"Final Training Accuracy: {training_accuracy:.2f}%")
 
 
-model.eval()
+# Test accuracy
 
 correct_predictions_test = 0
 total_predictions_test = 0
 
 with torch.no_grad():
     for batch_number, (images, labels) in enumerate(tqdm(test_dataloader)):
+        images = images.to(device)
+        labels = labels.to(device)
         output = model(images)
         predicted_classes = output.argmax(dim=1)
        # print(images.shape)  [batch size, 3, 128, 128]
