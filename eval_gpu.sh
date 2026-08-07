a654ddf6b880a47572e291300ff19a44ac4d023d
N.Gandhi <ng3n18@loginX001.cluster.local>
2026-08-07 11:32:52 +0100

    Add command to run eval_transfer_learning.py

--- eval_gpu.sh
+++ eval_gpu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -l
-
 #SBATCH --job-name=catsdogs-eval
 #SBATCH --partition=l4,swarm_l4,a100
 #SBATCH --gres=gpu:1
@@ -9,5 +8,8 @@
 #SBATCH --time=01:00:00
 #SBATCH --output=catsdogs-%j.log
 
+set -euo pipefail
+
 cd "$SLURM_SUBMIT_DIR"
 $HOME/.conda/envs/catsdogs-iridis/bin/python src/eval.py
+$HOME/.conda/envs/catsdogs-iridis/bin/python src/eval_transfer_learning.py
